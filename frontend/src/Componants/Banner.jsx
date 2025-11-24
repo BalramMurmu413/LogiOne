cf60083933cae5ba25a4372d2cbffb1d8121bb4b
balramm413 <balramm413@gmail.com>
2025-11-24 19:52:11 +0530

    first commit

---
+++ frontend/src/Componants/Banner.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+
+export default function Banner() {
+  return (
+    <>
+    <div className='w-[96%] md:ml-32 flex-col h-[420px] m-auto  md:flex-row justify-around lg:items-center flex'>
+
+
+     
+
+        <span className="items-center text-rotate text-7xl">
+  <span className="justify-items-center">
+    <span>DESIGN</span>
+    <span>DEVELOP</span>
+    <span>DEPLOY</span>
+    <span>SCALE</span>
+    <span>MAINTAIN</span>
+    <span>REPEAT</span>
+  </span>
+</span>
+{/* ========================================================= */}
+
+
+
+
+{/* =========================================================== */}
+
+<a href="#" className="hover-3d my-12 mx-2 cursor-pointer">
+  
+  {/* content */}
+  <div className="card w-96  text-black bg-[radial-gradient(circle_at_bottom_left,#ffffff04_90%,transparent_36%),radial-gradient(circle_at_top_right,#ffffff04_70%,transparent_3%)] bg-size-[4.95em_4.95em]">
+    <div className="card-body">
+      <div className="flex justify-between mb-10">
+        <div className="text-5xl opacity-10">❁</div>
+      </div>
+        <div className=" m-auto text-5xl font-bold">Hellow there</div>
+      <div className="text-lg mb-4 opacity-40"> "We create clean, efficient, and customized web applications that deliver real results."</div>
+      <div className="flex justify-between">
+        <div>
+
+        </div>
+      </div>
+        <div>
+    </div>
+           <button className="btn ">Get Started</button>
+        </div>
+  </div>
+  
+  {/* 8 empty divs needed for the 3D effect */}
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+</a>
+ {/* ?[[[[[[[]]]]]]] */}
+
+
+
+
+
+
+
+<div className='m-auto'>
+    <figure className="hover-gallery max-w-60 rounded-r-4xl">
+  <img src="https://img.daisyui.com/images/stock/daisyui-hat-1.webp" />
+  <img src="https://img.daisyui.com/images/stock/daisyui-hat-2.webp" />
+  <img src="https://img.daisyui.com/images/stock/daisyui-hat-3.webp" />
+  <img src="https://img.daisyui.com/images/stock/daisyui-hat-4.webp" />
+</figure>
+</div>
+    </div>
+    
+    </>
+  )
+}
---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Componants/Header'
+import Banner from '../Componants/Banner'
+
+export default function Home() {
+  return (
+    <div>
+        <Header/>
+        <Banner/>
+
+    </div>
+  )
+}
